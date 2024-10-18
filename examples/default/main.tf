5cbc7dc5f77ba6f1d22087ed7b6701e3105a32d9
Isabelle Bersano <ibersano@microsoft.com>
2024-10-18 16:11:46 -0400

    fix: update default example

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.74"
+      version = ">=4.0.0"
     }
     modtm = {
       source  = "azure/modtm"
@@ -51,13 +51,14 @@ resource "azurerm_resource_group" "this" {
 # Do not specify location here due to the randomization above.
 # Leaving location as `null` will cause the module to use the resource group location
 # with a data source.
-module "test" {
-  source = "../../"
-  # source             = "Azure/avm-<res/ptn>-<name>/azurerm"
-  # ...
-  location            = azurerm_resource_group.this.location
-  name                = "TODO" # TODO update with module.naming.<RESOURCE_TYPE>.name_unique
+module "default" {
+  source  = "../.."
+  name = module.naming.kubernetes_cluster.name_unique
   resource_group_name = azurerm_resource_group.this.name
-
-  enable_telemetry = var.enable_telemetry # see variables.tf
+  location = azurerm_resource_group.this.location
+  default_node_pool = {
+    name = "default"
+    vm_size = "Standard_DS2_v2"
+    node_count = 3
+  }
 }
