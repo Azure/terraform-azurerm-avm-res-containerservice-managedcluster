fc449ca6cdb8fde14230ac0bd9feec71eabd84cf
Isabelle Bersano <ibersano@microsoft.com>
2024-10-18 16:11:24 -0400

    feat: add automatic example

---
+++ examples/automatic/main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_version = "~> 1.5"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=4.0.0"
+    }
+    modtm = {
+      source  = "azure/modtm"
+      version = "~> 0.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+## Section to provide a random Azure region for the resource group
+# This allows us to randomize the region for the resource group.
+module "regions" {
+  source  = "Azure/avm-utl-regions/azurerm"
+  version = "~> 0.1"
+}
+
+# This allows us to randomize the region for the resource group.
+resource "random_integer" "region_index" {
+  max = length(module.regions.regions) - 1
+  min = 0
+}
+## End of section to provide a random Azure region for the resource group
+
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "~> 0.3"
+}
+
+# This is required for resource modules
+resource "azurerm_resource_group" "this" {
+  location = module.regions.regions[random_integer.region_index.result].name
+  name     = module.naming.resource_group.name_unique
+}
+
+module "automatic" {
+  source  = "../.."
+  name = module.naming.kubernetes_cluster.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+  location = azurerm_resource_group.this.location
+
+  default_node_pool = {
+    name = "default"
+    vm_size = "Standard_DS2_v2"
+    node_count = 3
+  }
+
+  maintenance_window_auto_upgrade = {
+    frequency = "Weekly"
+    interval = "1"
+    day_of_week = "Sunday"
+    duration = 4
+    utc_offset = "+00:00"
+    start_time = "00:00"
+    start_date = "2024-10-15"
+  }
+  
+}
