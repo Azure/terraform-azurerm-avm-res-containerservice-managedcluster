1c7a04a1c4ef3b6dc73bbc761349c9c73071aaf0
Isabelle Bersano <ibersano@microsoft.com>
2024-10-18 16:00:57 -0400

    feat: add azure cni example

---
+++ examples/azure-cni/main.tf
@@ -0,0 +1,183 @@
+terraform {
+  required_version = "~> 1.5"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=4.0.0"
+    }
+    modtm = {
+      source  = "azure/modtm"
+      version = "~> 0.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+## Section to provide a random Azure region for the resource group
+# This allows us to randomize the region for the resource group.
+module "regions" {
+  source  = "Azure/avm-utl-regions/azurerm"
+  version = "~> 0.1"
+}
+
+# This allows us to randomize the region for the resource group.
+resource "random_integer" "region_index" {
+  max = length(module.regions.regions) - 1
+  min = 0
+}
+## End of section to provide a random Azure region for the resource group
+
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "~> 0.3"
+}
+
+# This is required for resource modules
+resource "azurerm_resource_group" "this" {
+  location = module.regions.regions[random_integer.region_index.result].name
+  name     = module.naming.resource_group.name_unique
+}
+
+
+resource "azurerm_virtual_network" "vnet" {
+  name = "cni-vnet"
+  resource_group_name = azurerm_resource_group.this.name
+  location = azurerm_resource_group.this.location
+  address_space = ["10.1.0.0/16"]
+}
+
+resource "azurerm_subnet" "default_subnet" {
+  name = "default"
+  resource_group_name = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = ["10.1.0.0/24"]
+}
+
+resource "azurerm_subnet" "unp1_subnet" {
+  name = "unp1"
+  resource_group_name = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = ["10.1.1.0/24"]
+}
+
+resource "azurerm_subnet" "unp2_subnet" {
+  name = "unp2"
+  resource_group_name = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = ["10.1.2.0/24"]
+}
+
+resource "azurerm_private_dns_zone" "zone" {
+  name = "privatelink.${azurerm_resource_group.this.location}.azmk8s.io"
+  resource_group_name = azurerm_resource_group.this.name  
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "vnetLink" {
+  name = "privatelink-${azurerm_resource_group.this.location}-azmk8s-io"
+  private_dns_zone_name = azurerm_private_dns_zone.zone.name
+  resource_group_name = azurerm_resource_group.this.name
+  virtual_network_id = azurerm_virtual_network.vnet.id
+}
+
+resource "azurerm_log_analytics_workspace" "workspace" {
+  name                = "azure-cni-log-analytics"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+module "kubenet" {
+  source  = "../.."
+  name = module.naming.kubernetes_cluster.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+  location = azurerm_resource_group.this.location
+  default_node_pool = {
+    name = "default"
+    vm_size = "Standard_DS2_v2"
+    node_count = 1
+    vnet_subnet_id = azurerm_subnet.default_subnet.id
+  }
+
+  network_profile = {
+    network_plugin = "azure"
+    network_data_plane = "azure"
+    network_plugin_mode = "overlay"
+  }
+
+  node_pools = [
+    {
+      name = "userpool1"
+      vm_size = "Standard_DS2_v2"
+      node_count = 2
+      zones = [ 3 ]
+      auto_scaling_enabled = true
+      max_count = 3
+      max_pods = 30
+      min_count = 1
+      os_disk_size_gb = 128
+      vnet_subnet_id = azurerm_subnet.unp1_subnet.id
+    },
+    {
+      name = "userpool2"
+      vm_size = "Standard_DS2_v2"
+      node_count = 2
+      zones = [ 3 ]
+      auto_scaling_enabled = true
+      max_count = 3
+      max_pods = 30
+      min_count = 1
+      os_disk_size_gb = 128
+      vnet_subnet_id = azurerm_subnet.unp2_subnet.id
+    }
+  ]
+
+  automatic_upgrade_channel = "stable"
+  node_os_channel_upgrade = "Unmanaged"
+
+  maintenance_window_auto_upgrade = {
+    frequency = "Weekly"
+    interval = "1"
+    day_of_week = "Sunday"
+    duration = 4
+    utc_offset = "+00:00"
+    start_time = "00:00"
+    start_date = "2024-10-15"
+  }
+
+  maintenance_window_node_os = {
+    frequency = "Weekly"
+    interval = "1"
+    day_of_week = "Sunday"
+    duration = 4
+    utc_offset = "+00:00"
+    start_time = "00:00"
+    start_date = "2024-10-15"
+  }
+
+  workload_identity_enabled = true
+  oidc_issuer_enabled = true
+
+  open_service_mesh_enabled = true
+  storage_profile = {
+    blob_driver_enabled = true
+    disk_driver_enabled = true
+    file_driver_enabled = true
+    snapshot_controller_enabled = true
+  }
+
+  oms_agent = {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.workspace.id
+  }
+
+  defender_log_analytics_workspace_id = azurerm_log_analytics_workspace.workspace.id  
+}
