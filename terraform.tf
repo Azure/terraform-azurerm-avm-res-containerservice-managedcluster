12c7d22cf2a689daec301d48a9c4a9f8a9f97e17
hezijie <lonegunmanb@hotmail.com>
2024-11-27 15:56:48 +0800

    some changes

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">= 1.9.2"
   required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~>2.0"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "< 4.0.0"
@@ -11,15 +15,7 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5"
-    }
-    azapi = {
-      source  = "Azure/azapi"
-      version = ">=2.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.0"
+      version = ">= 3.5.0, < 4.0.0"
     }
   }
 }
