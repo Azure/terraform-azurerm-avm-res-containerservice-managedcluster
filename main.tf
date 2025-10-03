15942259a1a159902813b5103bfc31c8179d34d9
Min Hein Aung <minheinaung97@gmail.com>
2025-10-03 03:41:56 +0000

    add new nested variables for api_server_access_profile profile block for azurerm_kubernetes_cluster resource

--- main.tf
+++ main.tf
@@ -172,7 +172,9 @@ resource "azurerm_kubernetes_cluster" "this" {
     for_each = var.api_server_access_profile != null ? [var.api_server_access_profile] : []
 
     content {
-      authorized_ip_ranges = api_server_access_profile.value.authorized_ip_ranges
+      authorized_ip_ranges                = api_server_access_profile.value.authorized_ip_ranges
+      subnet_id                           = api_server_access_profile.value.subnet_id
+      virtual_network_integration_enabled = api_server_access_profile.value.virtual_network_integration_enabled
     }
   }
   # Auto Scaler Configuration
