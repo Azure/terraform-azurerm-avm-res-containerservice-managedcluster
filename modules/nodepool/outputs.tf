c0a043ab793c7c4c4bce881b54c9055f157d25d4
Frank Pejril <frank.pejril@gmail.com>
2026-01-10 16:51:25 -0600

    feat: add full resource outputs for cluster and node pools

--- modules/nodepool/outputs.tf
+++ modules/nodepool/outputs.tf
@@ -3,6 +3,11 @@ output "name" {
   value       = var.create_nodepool_before_destroy ? azurerm_kubernetes_cluster_node_pool.create_before_destroy_node_pool[0].name : azurerm_kubernetes_cluster_node_pool.this[0].name
 }
 
+output "resource" {
+  description = "The node pool resource."
+  value       = var.create_nodepool_before_destroy ? azurerm_kubernetes_cluster_node_pool.create_before_destroy_node_pool : azurerm_kubernetes_cluster_node_pool.this
+}
+
 output "resource_id" {
   description = "Resource ID of the Kubernetes cluster."
   value       = var.create_nodepool_before_destroy ? azurerm_kubernetes_cluster_node_pool.create_before_destroy_node_pool[0].id : azurerm_kubernetes_cluster_node_pool.this[0].id
