0824459f885f14eba4e9c609eb9b13f94e979af5
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2026-02-24 16:55:04 +0000

    core: pre commit

--- modules/namespace/main.tf
+++ modules/namespace/main.tf
@@ -1,11 +1,11 @@
 resource "azapi_resource" "this" {
-  name                   = var.name
-  parent_id              = var.parent_id
-  type                   = "Microsoft.ContainerService/managedClusters/managedNamespaces@2025-10-01"
-  body                   = local.resource_body
-  response_export_values = []
-  tags                   = var.tags
+  name      = var.name
+  parent_id = var.parent_id
+  type      = "Microsoft.ContainerService/managedClusters/managedNamespaces@2025-10-01"
+  body      = local.resource_body
   locks = [
     var.parent_id
   ]
+  response_export_values = []
+  tags                   = var.tags
 }
